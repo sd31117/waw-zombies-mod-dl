3f672b82224c5fdb7288e0eb5de464713c2dad4c
mshriver2 <maxwell.shriver@yahoo.com>
2020-11-12 01:05:10 -0700

    added modurl input

--- download.sh
+++ download.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+#collects user input
 echo "Welcome to the Linux COD Zombies WAW Custom Map Downloader!\n"
 
-echo "Please enter the path to your waw mods folder dir [press enter for default]: "
+echo "Please enter the path to your waw mods folder dir [press enter for default]: \n"
 
+#stores cod mod folder path
 read modPath
+
+echo "Enter the url to the TAR.GZ containg the mod folders you wish to install: "
+
+#stores url to mod tar.gz
+read modURL 
