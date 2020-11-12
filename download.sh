074922c448ef64c8db239c2e692cd5fb92bfd882
mshriver2 <maxwell.shriver@yahoo.com>
2020-11-12 01:00:10 -0700

    added first user inputs

---
+++ download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Welcome to the Linux COD Zombies WAW Custom Map Downloader!\n"
+
+echo "Please enter the path to your waw mods folder dir [press enter for default]: "
+
+read modPath
